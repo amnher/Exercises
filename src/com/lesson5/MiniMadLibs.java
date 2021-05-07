a5915ae120cf02bb218ebe461e6ac120b1d3d9c2
Herman Cruz <amnher@gmail.com>
2021-05-07 15:40:55 +0800

    Test some code on Lesson 5

--- src/com/lesson5/MiniMadLibs.java
+++ src/com/lesson5/MiniMadLibs.java
@@ -1,4 +1,17 @@
 package com.lesson5;
+import java.util.Scanner;
 
 public class MiniMadLibs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String stringInput = "";
+        int input = 0;
+
+        System.out.print("Enter a number: ");
+        input = Integer.parseInt(stringInput = sc.nextLine());
+        if (input >= 0)
+            System.out.println("The number is positive");
+        else
+            System.out.println("The number is negative");
+    }
 }
