6407cd05ab7a0732dc52b2b8314e91cd17bd2835
Herman Cruz <amnher@gmail.com>
2021-05-12 10:28:25 +0800

    Added Lesson 8 exercises Fizz Buzz

--- src/com/lesson8/FizzBuzz.java
+++ src/com/lesson8/FizzBuzz.java
@@ -4,7 +4,26 @@ public class FizzBuzz {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number;
+        int counter = 0;
+        System.out.print("how many units of fizzing and buzzing do you need in your life? ");
+        number = sc.nextInt();
 
+        for (int i = 0 ; counter < number ; i++) {
+            if (i != 0 && ((i%3==0) && (i%5==0))) {
+                System.out.println("fizz buzz");
+                counter++;
+            } else if (i != 0 && (i % 3 == 0)) {
+                System.out.println("fizz");
+                counter++;
+            } else if (i != 0 && (i % 5 ==0)) {
+                System.out.println("buzz");
+                counter++;
+            } else {
+                System.out.println(i);
+            }
+
+        }
+        System.out.println("TRADITION!!!!!");
 
     }
 }
