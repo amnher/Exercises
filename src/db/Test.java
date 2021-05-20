9d58eec4db5ebe69609bc999886e262c2d5ad8f5
Herman Cruz <amnher@gmail.com>
2021-05-20 18:01:58 +0800

    Added Lesson 9 exercise and Intro to DB Connectivity

---
+++ src/db/Test.java
@@ -0,0 +1,25 @@
+package db;
+import java.sql.*;
+public class Test {
+    public static void main(String[] args) {
+        getConnection();
+    }
+    private static Connection getConnection()
+    {
+        Connection con = null;
+        try
+        {
+            String url = "jdbc:mysql://localhost/Movies";
+            String user = "root";
+            String pw = "00000000";
+            con = DriverManager.getConnection(url, user, pw);
+            System.out.println("Success");
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        return con;
+    }
+}
